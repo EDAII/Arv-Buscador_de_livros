ee7e92dea8655ebc8b780875ce46cbe5e4429573
Mayara Alves de Oliveira <67807684+Mayara-tech@users.noreply.github.com>
2025-11-10 21:07:28 -0300

    Delete arvore directory

--- arvore/node.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, key, data, color="RED"):
-        self.key = key
-        self.data = data
-        self.color = color
-        self.left = None
-        self.right = None
-        self.parent = None
